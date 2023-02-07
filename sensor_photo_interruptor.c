9d9386b592a2ab312698f446b8e2e29196e760c3
GustavoAPS <gugustavo137@gmail.com>
2023-02-07 20:26:21 -0300

    Adicionando sensor photo interruptor

---
+++ sensor_photo_interruptor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "esp_log.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define PIN_PHOTO_INTERRUPTOR 15  // GPIO15
+
+void app_main()
+{
+    int state = 0;
+    ESP_LOGI("Photo-interrupter", "Starting...");
+    esp_rom_gpio_pad_select_gpio(PIN_PHOTO_INTERRUPTOR);
+    gpio_set_direction(PIN_PHOTO_INTERRUPTOR, GPIO_MODE_INPUT);
+
+    while(1)
+    {
+        state = gpio_get_level(PIN_PHOTO_INTERRUPTOR);
+        if (state == 0)
+            ESP_LOGI("Photo-interrupter", "Sensor detected something");
+        else
+            ESP_LOGI("Photo-interrupter", "No sensor detected");
+        vTaskDelay(pdMS_TO_TICKS(300));
+    }
+}
